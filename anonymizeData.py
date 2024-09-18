333290bf1073f2c1ca7008cd3978f90df31d202b
josura <giorgiolocicero@hotmail.com>
2024-09-18 11:53:27 +0200

    python code to drop relevant info about the person

---
+++ anonymizeData.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+notAnonymizedData = pd.read_csv('filtered-surveys.csv')
+
+# Drop the column with the names and the email addresses
+anonymizedData = notAnonymizedData.drop('name', axis=1)
+anonymizedData = anonymizedData.drop('email', axis=1)
+anonymizedData = anonymizedData.drop('birthdate', axis=1)
+anonymizedData = anonymizedData.drop('emailPerLestrazione', axis=1)
+
+# Save the anonymized data to a new file
+anonymizedData.to_csv('anonymized-surveys.csv', index=False)
