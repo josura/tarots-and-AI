07acbea4ee78b06046f94c9963e05b647f1e9589
josura <giorgiolocicero@hotmail.com>
2024-09-22 20:36:05 +0200

    testing plots for cluster analysis and features in common with the clusters

--- anonymizeData.py
+++ anonymizeData.py
@@ -5,7 +5,6 @@ notAnonymizedData = pd.read_csv('filtered-surveys.csv')
 # Drop the column with the names and the email addresses
 anonymizedData = notAnonymizedData.drop('name', axis=1)
 anonymizedData = anonymizedData.drop('email', axis=1)
-anonymizedData = anonymizedData.drop('birthdate', axis=1)
 anonymizedData = anonymizedData.drop('emailPerLestrazione', axis=1)
 
 # Save the anonymized data to a new file
